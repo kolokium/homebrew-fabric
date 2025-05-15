72410974fde38474a39cf83197b2d87fbbe01eae
Javier Ruiz <japi@kolokium.com>
2025-05-15 10:53:23 +0200

    feat: add 3.1.1 and update to 2.5.13, 2.2.15

--- Formula/hyperledger-fabric-cli@2.2.15.rb
+++ Formula/hyperledger-fabric-cli@2.2.15.rb
@@ -1,22 +1,20 @@
-class HyperledgerFabricCliAT256 < Formula
+class HyperledgerFabricCliAT225 < Formula
   desc "Hyperledger Fabric is an enterprise-grade permissioned distributed ledger"
   homepage "https://www.hyperledger.org/use/fabric"
-  url "https://github.com/hyperledger/fabric/archive/refs/tags/v2.5.6.tar.gz"
-  version "2.5.6"
-  sha256 "44a694315bc332f8a005910b0affbd5b7daf7721a2501c904a45ee9a49dfb028"
+  url "https://github.com/hyperledger/fabric/archive/refs/tags/v2.2.15.tar.gz"
+  version "2.2.15"
+  sha256 "c6b7e7f3362620210588193c107ea6e56745510c50394f19553c7840fa1344ff"
   license "Apache-2.0"
 
   def install
-    system "./scripts/install-fabric.sh", "binary"
+    system "./scripts/bootstrap.sh", "-d", "-s"
     bin.install "bin/configtxgen"
     bin.install "bin/configtxlator"
     bin.install "bin/cryptogen"
     bin.install "bin/discover"
     bin.install "bin/fabric-ca-client"
     bin.install "bin/fabric-ca-server"
-    bin.install "bin/ledgerutil"
     bin.install "bin/orderer"
-    bin.install "bin/osnadmin"
     bin.install "bin/peer"
   end
 
@@ -27,9 +25,7 @@ class HyperledgerFabricCliAT256 < Formula
     system "test", "-e", "#{bin}/discover"
     system "test", "-e", "#{bin}/fabric-ca-client"
     system "test", "-e", "#{bin}/fabric-ca-server"
-    system "test", "-e", "#{bin}/ledgerutil"
     system "test", "-e", "#{bin}/orderer"
-    system "test", "-e", "#{bin}/osnadmin"
     system "test", "-e", "#{bin}/peer"
   end
 end
--- Formula/hyperledger-fabric-cli@2.5.13.rb
+++ Formula/hyperledger-fabric-cli@2.5.13.rb
@@ -1,20 +1,22 @@
-class HyperledgerFabricCliAT225 < Formula
+class HyperledgerFabricCliAT256 < Formula
   desc "Hyperledger Fabric is an enterprise-grade permissioned distributed ledger"
   homepage "https://www.hyperledger.org/use/fabric"
-  url "https://github.com/hyperledger/fabric/archive/refs/tags/v2.2.5.tar.gz"
-  version "2.2.5"
-  sha256 "aba2ccee8fe5ee019cd8fa110c611c66d4754ac3d48a7226d8b46aeb784cea37"
+  url "https://github.com/hyperledger/fabric/archive/refs/tags/v2.5.13.tar.gz"
+  version "2.5.13"
+  sha256 "c3f24a3f0333878f7a59617f8d667659c0b686eb4e4b6b98f553feec66d4089f"
   license "Apache-2.0"
 
   def install
-    system "./scripts/bootstrap.sh", "-d", "-s"
+    system "./scripts/install-fabric.sh", "binary"
     bin.install "bin/configtxgen"
     bin.install "bin/configtxlator"
     bin.install "bin/cryptogen"
     bin.install "bin/discover"
     bin.install "bin/fabric-ca-client"
     bin.install "bin/fabric-ca-server"
+    bin.install "bin/ledgerutil"
     bin.install "bin/orderer"
+    bin.install "bin/osnadmin"
     bin.install "bin/peer"
   end
 
@@ -25,7 +27,9 @@ class HyperledgerFabricCliAT225 < Formula
     system "test", "-e", "#{bin}/discover"
     system "test", "-e", "#{bin}/fabric-ca-client"
     system "test", "-e", "#{bin}/fabric-ca-server"
+    system "test", "-e", "#{bin}/ledgerutil"
     system "test", "-e", "#{bin}/orderer"
+    system "test", "-e", "#{bin}/osnadmin"
     system "test", "-e", "#{bin}/peer"
   end
 end
